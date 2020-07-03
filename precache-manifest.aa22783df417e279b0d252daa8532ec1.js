368706b6289f90050376770ccbd9227322a0b5a6
Jee Zhou <jwhiz113@gmail.com>
2020-07-03 17:32:40 -0500

    Updates

---
+++ precache-manifest.aa22783df417e279b0d252daa8532ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71d8b22dbc4f255ad772411ade2bec7",
+    "url": "/reacthw19/index.html"
+  },
+  {
+    "revision": "9dcd1f507c5ef76a673e",
+    "url": "/reacthw19/static/js/2.8c8525a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthw19/static/js/2.8c8525a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99458c0c78c7379468a1",
+    "url": "/reacthw19/static/js/main.3435d649.chunk.js"
+  },
+  {
+    "revision": "60a72e3d762f92b559f3",
+    "url": "/reacthw19/static/js/runtime-main.6180f8f4.js"
+  }
+]);
